301ead5b392713858347f36749c2771e8b997ab9
Jeevitha94DSA <jeevithavignesh2001@gmail.com>
2025-09-24 16:14:45 +0530

    Task 2

--- src/Task1/Atmimplementation.java
+++ src/Task1/Atmimplementation.java
@@ -1,6 +1,6 @@
 package Task1;
 
-import java.sql.*;
+import  java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 
--- src/Task1/DBUtil.java
+++ src/Task1/DBUtil.java
@@ -14,7 +14,7 @@ public class DBUtil {
 //    }
     private static final String URL = "jdbc:mysql://localhost:3306/account_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "P@ssw0rd";
+    private static final String PASSWORD = "----";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
